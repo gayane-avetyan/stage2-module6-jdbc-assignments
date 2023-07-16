005e5cf4d98b3759d6dc47aaf2bc476b7cd5d1ec
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-17 02:15:55 +0400

    deleteUser added public access

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -181,7 +181,7 @@ public class SimpleJDBCRepository {
 
     }
 
-    private void deleteUser(Long userId) throws SQLException {
+    public void deleteUser(Long userId) throws SQLException {
         try {
             connection = CustomDataSource.getInstance().getConnection();
             ps = connection.prepareStatement(deleteUser);
